3423b380b89d4456bed6fca67e80cf28c2a75eda
fcosta97 <filipemac2007@gmail.com>
2023-07-13 21:53:04 +0100

    image updates

--- configs.py
+++ configs.py
@@ -2,18 +2,20 @@ import pygame
 pygame.init()
 
 class Window:
-    WIDTH = 1000
-    HEIGHT = 750
+    WIDTH = 1720
+    HEIGHT = 988
     TITLE = "TEST"
     # ICON = pygame.image.load("imgs/icon.png")
 
+pygame.display.set_mode((Window.WIDTH, Window.HEIGHT))
+
 class World:
-    BACKGROUND = pygame.image.load("imgs/bg.png")
+    BACKGROUND = pygame.image.load("imgs/bg.png").convert_alpha()
 
 class Skins:
-    PLAYER = pygame.image.load("imgs/player.png")
-    ENEMY = pygame.image.load("imgs/enemy.png")
-    FRUIT = pygame.image.load("imgs/fruit.png")
+    PLAYER = pygame.image.load("imgs/player.png").convert_alpha()
+    ENEMY = pygame.image.load("imgs/enemy.png").convert_alpha()
+    FRUIT = pygame.image.load("imgs/fruit.png").convert_alpha()
 
 # class Font:
     # MAIN = pygame.font.Font('fonts/Roboto-Regular.ttf', 50)
