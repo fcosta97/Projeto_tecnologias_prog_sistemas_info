bc1e0b4ad72d822d156bf64459790ef66ba826a4
fcosta97 <filipemac2007@gmail.com>
2023-07-13 23:03:41 +0100

    added camera (moving background)

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ pygame.display.set_caption(Window.TITLE)
 player = Player()
 
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 144
+
+# tiles = math.ceil(Window.WIDTH / World.BACKGROUND_WIDTH) + math.ceil(Window.HEIGHT / World.BACKGROUND_HEIGHT)
+tiles = 2
+
+print(tiles)
 
 background_x = 0
 background_y = 0
+velocity = 3
 
 while 1:
     dt = clock.tick(FPS)
@@ -23,23 +29,35 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    screen.blit(World.BACKGROUND, [background_x, background_y])
+    # BACKGROUND
+    for i in range(0, tiles):
+        screen.blit(World.BACKGROUND, (i * World.BACKGROUND_WIDTH + background_x, background_y)) # right
+        screen.blit(World.BACKGROUND, (i * World.BACKGROUND_WIDTH + background_x, World.BACKGROUND_HEIGHT + background_y)) # down right
+        screen.blit(World.BACKGROUND, (i * World.BACKGROUND_WIDTH + background_x, -World.BACKGROUND_HEIGHT + background_y)) # up right
 
+        screen.blit(World.BACKGROUND, (i * -World.BACKGROUND_WIDTH + background_x, background_y)) # left
+        screen.blit(World.BACKGROUND, (i * -World.BACKGROUND_WIDTH + background_x, World.BACKGROUND_HEIGHT + background_y)) # down left
+        screen.blit(World.BACKGROUND, (i * -World.BACKGROUND_WIDTH + background_x, -World.BACKGROUND_HEIGHT + background_y)) # up left
+
+    if abs(background_x) > World.BACKGROUND_WIDTH:
+        background_x = 0
+
+    if abs(background_y) > World.BACKGROUND_HEIGHT:
+        background_y = 0
+
+    # screen.blit(World.BACKGROUND, [background_x, background_y])
+
+    # CONTROLS
     key = pygame.key.get_pressed()
 
     if key[pygame.K_a] or key[pygame.K_LEFT]:
-        # player.move_left()
-        background_x -= 5
+        background_x += velocity
     if key[pygame.K_d] or key[pygame.K_RIGHT]:
-        # player.move_right()
-        background_x += 5
+        background_x -= velocity
     if key[pygame.K_w] or key[pygame.K_UP]:
-        # player.move_up()
-        background_y -= 5
+        background_y += velocity
     if key[pygame.K_s] or key[pygame.K_DOWN]:
-        # player.move_down()
-        background_y += 5
-
+        background_y -= velocity
 
     player.draw(screen)
 
--- player.py
+++ player.py
@@ -3,23 +3,23 @@ from configs import *
 
 class Player:
     def __init__(self):
-        self.__x = Window.WIDTH / 2
-        self.__y = Window.HEIGHT / 2
+        self.__x = (Window.WIDTH / 2) - (Skins.PLAYER.get_width() / 2)
+        self.__y = (Window.HEIGHT / 2) - (Skins.PLAYER.get_height() / 2)
         self.__img = Skins.PLAYER
         self.__health = 0
         self.__score = 0
 
-    def move_up(self):
-        self.__y -= 5 
+    # def move_up(self):
+    #     self.__y -= 5 
 
-    def move_down(self):
-        self.__y += 5
+    # def move_down(self):
+    #     self.__y += 5
 
-    def move_left(self):
-        self.__x -= 5
+    # def move_left(self):
+    #     self.__x -= 5
 
-    def move_right(self):
-        self.__x += 5
+    # def move_right(self):
+    #     self.__x += 5
 
     def draw(self, surface):
         surface.blit(self.__img, [self.__x, self.__y])
